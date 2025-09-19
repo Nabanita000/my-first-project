a6d875b0ce613ccb090c6cce696a467cf92b6d2d
Nabanita Shit <nabanitashit809@gmail.com>
2025-09-19 20:31:31 +0000

    datatypes

---
+++ all/dataTypes.js
@@ -0,0 +1,16 @@
+let name="Nabanita"
+let age = 19
+let isLoggedIn=false
+let state;
+//number=>2 to power 53
+//bigInt
+//string=" "
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+console.log(typeof("naba"));
+console.log(typeof null);
+console.log(typeof undefined);
